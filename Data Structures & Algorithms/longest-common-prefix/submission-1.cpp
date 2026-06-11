f32c995f3e9fa2fed0c699ce7e357487c3ea394a
Luiz Felipe <lutilipe02@hotmail.com>
2026-06-11 09:00:18 -0300

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,40 @@
+class TrieNode {
+    public:
+        TrieNode* children[26];
+        int count = 0;
+};
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root = new TrieNode();
+        root->count = strs.size();
+
+        for (string s : strs) {
+            TrieNode* curr = root;
+            for (char c : s) {
+                int idx = c - 'a';
+                if (!curr->children[idx]) {
+                    curr->children[idx] = new TrieNode();
+                }
+                curr = curr->children[idx];
+                curr->count++;
+            }
+        }
+
+        string first = strs[0];
+        string ans = "";
+        TrieNode* curr = root;
+
+        for (char c : first) {
+            int idx = c - 'a';
+            if (!curr->children[idx]) break;
+            cout << curr->children[idx]->count << endl;
+            if (curr->children[idx]->count < strs.size()) break;
+            ans+=c;
+            curr = curr->children[idx];
+        }
+
+        return ans;
+    }
+};
